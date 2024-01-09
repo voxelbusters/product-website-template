53eb8a7f9c33ef4eb510aeb3e86ed03278564045
avezqureshi14 <aveznqureshi14@gmail.com>
2024-01-09 17:33:51 +0530

    fix: updated site name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'ads KIT',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/SmallIcon.png',
 
@@ -74,9 +74,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'ads KIT',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'ads KIT logo',
           src: 'img/SmallIcon.png',
         },
         items: [
