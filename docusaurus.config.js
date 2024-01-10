352ee00847504c3e139617a3a8695c1c129a5ea8
avezqureshi14 <aveznqureshi14@gmail.com>
2024-01-10 12:47:18 +0530

    fix(docs):  docusaurus.config cname

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/SmallIcon.png',
 
   // Set the production url of your site here
-  url: 'https://voxelbusters.github.io',
+  url: 'https://adskit.voxelbusters.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ads-kit-docs',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
